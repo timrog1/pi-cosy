96f736172c0e5418ff0ce9bf484f22021a414cf0
timrog1 <timro@interxion.com>
2017-01-04 17:45:40 +0000

    schedule array

--- sensor.js
+++ sensor.js
@@ -2,14 +2,12 @@
 var temp = require('ds18b20');
 let rx = require("rxjs");
 
-module.exports = rx.Observable.create(obs => 
-	temp.sensors((err, ids) => {
-		if (err || !ids || !ids[0]) obs.next(undefined);
-		else {
-			let id = ids[0];
-			let fetch = () => temp.temperature(id, (e, value) => obs.next(value));
-			rx.Observable.timer(0, 1000).subscribe(fetch);
-			fetch();
-		}
-	}))
-	.distinctUntilChanged();
+module.exports = rx.Observable.create(obs => {
+	let fetch = () => 
+		temp.sensors((err, ids) => 
+			err || !ids || !ids[0] 
+				? obs.next(undefined) 
+				: temp.temperature(ids[0], (e, value) => obs.next(value)));
+
+	rx.Observable.timer(0, 1000).subscribe(fetch);
+}).distinctUntilChanged();
